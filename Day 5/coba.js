4b37e55e48849115927e92349a8d60255df2412a
andrsjm <darkclaymoon@gmail.com>
2023-11-10 13:08:46 +0700

    add command for each function

--- Day 5/coba.js
+++ Day 5/coba.js
@@ -1,8 +1,47 @@
-const fs = require('fs');
+//Import library
+const validator = require('validator');
 
-fs.writeFileSync('coba.txt', 'wadidaw and digigaw');
+//Make readline
+const rli = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-fs.readFile('coba.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data)
-})
+//Make the function for question and answer
+const input = (question) => {
+    return new Promise((cb, err) => {
+        rli.question(question, data=> {
+            cb(data);
+        }, () => {
+            err()
+        });
+    });
+}
+
+
+//Make function to assist the console.log()
+const output = (answer, data) => {
+    console.log(answer, data)
+}
+
+
+//Make the validator for spesific question
+const tanyaLagi = async (question, answerRight, answerFalse, validator) => {
+    const reply = await input(question)
+    if(validator(reply)){
+        output(answerRight, reply)
+    }else{
+        output(answerFalse, reply)
+        await tanyaLagi(question, answerRight, answerFalse, validator)
+    }
+}
+
+//Main
+const main = async () => {
+    await tanyaLagi("Email kamu apa: ", "Format email kamu benar: ", "Format email kamu salah: ", validator.isEmail)
+    await tanyaLagi("Berapa nomer telefon kamu: ", "Format telepon kamu benar: ", "Format telepon kamu salah: ", validator.isMobilePhone)
+
+    rli.close();
+};
+
+main();
